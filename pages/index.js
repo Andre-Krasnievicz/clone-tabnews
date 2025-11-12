e4e261f0593f2f4a84fe814a76448ac9cfa87cd8
Andre Felipe Krasnievicz da Costa <161515694+Andre-Krasnievicz@users.noreply.github.com>
2025-11-12 23:56:49 +0000

    delete dir .next e node_modules

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ei, vida! Que o nosso amor seja como o tempo depois da tempestade — calmo, fresco e cheio de céu azul por dentro. Te amo❤️</h1>
+    return <h1>Ei, vida! Que o nosso amor seja como o tempo depois da tempestade — calmo, fresco e cheio de céu azul por dentro. Te amo❤️❤️</h1>
 }
 
 export default Home;
