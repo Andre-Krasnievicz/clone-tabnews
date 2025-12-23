3703bc7a779b002d03e945f60e762b9175da4ff7
Andre Krasnievicz <andrekrascosta@gmail.com>
2025-12-22 21:18:33 -0400

    refactor: changed the way database information is displayed

--- pages/status/index.js
+++ pages/status/index.js
@@ -21,29 +21,36 @@ function UpdatedAt() {
   return <div>Última atualização: {updatedAtText}</div>;
 }
 
-function DatabaseHealth() {
+function DatabaseStatus() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
   });
 
-  let versionDatabase = "Carregando...";
-  let maxConnectionsDatabase = "Carregando...";
-  let openedConnectionsDatabase = "Carregando...";
+  let databaseInformation = "Carregando...";
 
   if (!isLoading && data) {
-    versionDatabase = data.dependencies.database.version;
-    maxConnectionsDatabase = data.dependencies.database.max_connections;
-    openedConnectionsDatabase = data.dependencies.database.opened_connections;
+    databaseInformation = (
+      <>
+        <div>
+          <ul>
+            <li>Versão: {data.dependencies.database.version}</li>
+            <li>
+              Conexões máximas: {data.dependencies.database.max_connections}
+            </li>
+            <li>
+              Conexões abertas: {data.dependencies.database.opened_connections}
+            </li>
+          </ul>
+        </div>
+      </>
+    );
   }
 
   return (
-    <div>
-      <ul>
-        <li>Conexões máximas: {versionDatabase}</li>
-        <li>Conexões máximas: {maxConnectionsDatabase}</li>
-        <li>Conexões máximas: {openedConnectionsDatabase}</li>
-      </ul>
-    </div>
+    <>
+      <h2>Database</h2>
+      <div>{databaseInformation}</div>
+    </>
   );
 }
 
@@ -52,8 +59,7 @@ export default function StatusPage() {
     <>
       <h1>Status</h1>
       <UpdatedAt />
-      <h1>Database Health</h1>
-      <DatabaseHealth />
+      <DatabaseStatus />
     </>
   );
 }
